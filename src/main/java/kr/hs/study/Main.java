eb65afcf32a3d53b827ca44c0627a745186d55cc
Chaeyoung Lim <rnldyal1027@naver.com>
2023-04-05 12:19:39 +0900

    setter로 주입

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,5 +1,20 @@
-package kr.hs.study;public class Main {
+package kr.hs.study;
+
+import kr.hs.study.beans.TestBean1;
+import kr.hs.study.beans.TestBean2;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+
+        TestBean1 obj1 = ctx.getBean(TestBean1.class);
+        TestBean2 obj2 = ctx.getBean(TestBean2.class);
+//        obj2.setData1(11);
+//        obj2.setData2(11.11);
+
+        System.out.println("data1 : "+obj2.getData1());
+        System.out.println("data2 : "+obj2.getData2());
+
     }
 }
